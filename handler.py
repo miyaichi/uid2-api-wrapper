63e6e74b9fb8e5e5e1fd2526cc7b574647d97283
EC2 Default User <miyaichi@gmail.com>
2021-10-04 18:32:23 +0000

    Under development #5

--- handler.py
+++ handler.py
@@ -10,6 +10,9 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def get_secrets():
+    """
+    Retrieve secrets from AWS Secrets Manager.
+    """
     secret_name = os.environ["secret_name"]
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager",
@@ -19,13 +22,16 @@ def get_secrets():
 
 
 def ip_check(ip):
-    if os.environ["ip_white_list"]:
+    """
+    Check if the IP is on the whitelisted.
+    """
+    if os.environ["ip_white_list"] is not None:
         ip = ipaddress.ip_address(ip)
-        ip_white_list = [
+        networks = [
             ipaddress.ip_network(x.strip())
             for x in str(os.environ["ip_white_list"]).split(",")
         ]
-        for network in ip_white_list:
+        for network in networks:
             if ip in network:
                 return True
         return False
@@ -33,6 +39,17 @@ def ip_check(ip):
 
 
 def normalize_email(email):
+    """
+    Email Address Normalization.
+
+    1. Remove leading and trailing spaces.
+    2. Convert all ASCII characters to lowercase.
+    3. In gmail.com email addresses, remove the following characters from the username part of the email address:
+      1) The period (. (ASCII code 46)).
+         For example, normalize jane.doe@gmail.com to janedoe@gmail.com.
+      2) The plus sign (+ (ASCII code 43)) and all subsequent characters.
+         For example, normalize janedoe+home@gmail.com to janedoe@gmail.com.
+    """
     email = email.strip().lower()
     (user, domain) = email.split("@")
     if domain == "gmail.com":
@@ -42,10 +59,16 @@ def normalize_email(email):
 
 
 def base64_encoded_sha256(str):
+    """
+    Base64-encoded SHA256 hashe.
+    """
     return base64.b64encode(hashlib.sha256(str.encode()).digest()).decode()
 
 
 def handler_wrapper(func):
+    """
+    Decorator to enhance log output of handlers.
+    """
     def decorate(event, context):
         try:
             log = logging.getLogger()
@@ -64,6 +87,11 @@ def handler_wrapper(func):
 
 @handler_wrapper
 def uid2_sdk(event, context):
+    """
+    GET /uid2_sdk
+
+    Get the Client-Side SDK.
+    """
     env = Environment(loader=FileSystemLoader("./templates"), trim_blocks=True)
     template = env.get_template("uid2-sdk.tpl.js")
     base_url = "https://{}/{}".format(event["requestContext"]["domainName"],
@@ -84,6 +112,11 @@ def uid2_sdk(event, context):
 
 @handler_wrapper
 def token_generate(event, context):
+    """
+    GET /token/generate
+
+    Generate a UID2 token from an email address or hashed email address.
+    """
     if not ip_check(event["requestContext"]["identity"]["sourceIp"]):
         return {"status": "403", "statusDescription": "Forbidden"}
 
